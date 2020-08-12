9395d6bb6d1b15dc07584044063f535f5413ccfa
BoRiD10 <borid97@gmail.com>
2020-08-13 01:55:40 +0300

    Start

--- src/main/java/Agent.java
+++ src/main/java/Agent.java
@@ -13,18 +13,18 @@ public class Agent extends jade.core.Agent {
         }
         System.out.println(getLocalName() + " was Born ");
         //Добавление и запуск поведений
-        addBehaviour(new Behavior());
+//        addBehaviour(new Behavior());
 //        addBehaviour(new OneShotBehavior());
 //        addBehaviour(new TickerBehavior(this, 1000));
 //        addBehaviour(new WackerBehavior(this, 10000));
 
         //Обмен сообщениями
-//        if (getLocalName().equals("Mike")){
-//            addBehaviour(new SendMessageToJosh());
-//        } else if (getLocalName().equals("Josh")){
-//            addBehaviour(new SendMessageToMike());
-//        } else {
-//            System.out.println("Недопустимый агент");
-//        }
+        if (getLocalName().equals("Mike")){
+            addBehaviour(new SendMessageToJosh());
+        } else if (getLocalName().equals("Josh")){
+            addBehaviour(new SendMessageToMike());
+        } else {
+            System.out.println("Недопустимый агент");
+        }
     }
 }
